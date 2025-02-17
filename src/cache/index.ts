233181024408e27a434092f30eb18a7d5b2877ba
Derek Drill <derekdrillwebdev@gmail.com>
2025-02-17 17:32:21 -0500

    Fix cache issues (#9)
    * 🪲 ⚙️ fix cache setup errors
    
    * 🧹 removes memcached imp

--- src/cache/index.ts
+++ src/cache/index.ts
@@ -1,27 +1,23 @@
-import Memcached from 'memcached';
 import { Request, Response, NextFunction } from 'express';
 import memjs from 'memjs';
 
-const servers = process.env.MEMCACHIER_SERVERS || '';
-
-// const memcached = new Memcached(servers);
-
-const mc = memjs.Client.create(process.env.MEMCACHIER_SERVERS, {
+const memcached = memjs.Client.create(process.env.MEMCACHIER_SERVERS, {
   username: process.env.MEMCACHIER_USERNAME,
   password: process.env.MEMCACHIER_PASSWORD,
-  failover: true, // default: false
-  timeout: 1, // default: 0.5 (seconds)
-  keepAlive: true, // default: false
+  failover: true,
+  timeout: 1,
+  keepAlive: true,
 });
 
 const checkCache = (req: Request, res: Response, next: NextFunction) => {
-  console.log(req.originalUrl);
   const key = req.originalUrl;
-  mc.get(key, (err, cachedData) => {
+
+  memcached.get(key, (err, cachedData) => {
     if (err) {
       console.error(err);
       return next();
     }
+
     if (cachedData) {
       const data = JSON.parse(cachedData.toString());
       return res.json(data);
@@ -33,10 +29,10 @@ const checkCache = (req: Request, res: Response, next: NextFunction) => {
 const requestDataBasedOnCache = (req: Request, res: Response, data: any) => {
   const key = req.originalUrl;
 
-  mc.set(key, JSON.stringify(data), { expires: 60 }, (err) => {
+  memcached.set(key, JSON.stringify(data), { expires: 60 }, (err) => {
     if (err) console.error(err);
   });
   res.json(data);
 };
 
-export { checkCache, mc as memcached, requestDataBasedOnCache };
+export { checkCache, memcached, requestDataBasedOnCache };
