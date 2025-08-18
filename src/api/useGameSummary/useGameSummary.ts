d9cfc6604b7e565988ab8bbc11039547daf453ea
Derek Drill <derekdrillwebdev@gmail.com>
2025-08-18 00:38:30 -0400

    ✨ adds LLM-powered game summary (#47)

---
+++ src/api/useGameSummary/useGameSummary.ts
@@ -0,0 +1,27 @@
+import { ChatOpenAI } from '@langchain/openai';
+
+const useGameSummary = async ({ gameData }: { gameData: any }) => {
+  const model = new ChatOpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+    temperature: 0.5,
+  });
+
+  const gameSummaryPrompt = `You are a journalist and data analyst who covers the National Basketball Association passionately.
+    You are an expert at taking JSON data from a game and articulating a thorough and intriguing analysis of the game.
+    You will be provided JSON data which will consist of "homeTeam" and "visitorTeam" objects, containing 
+    box score data, stat leaders and the final score. You will analyze this data, detect any patterns, do any research, 
+    and return a summary of how this game went. 
+    Include these 3 sections: "Overview" of the game/score/outcome, "Key players" to highlight stat lines/performances, 
+    and then wrap it all up with "Insight" of why and how the game went the way it did (this is where your journalism skills should shine).
+    Here is this game's JSON data: ${gameData}.
+    Please return this data as the following JSON object: { overview: string; keyHighlights: string[]; insight: string }. 
+    Please wrap player names and stat values in strong tags in the keyHighlights.
+    `;
+
+  const gameSummary = JSON.parse(JSON.stringify(await model.invoke(gameSummaryPrompt))).kwargs
+    .content;
+
+  return JSON.parse(gameSummary);
+};
+
+export { useGameSummary };
