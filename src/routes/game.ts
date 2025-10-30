9faf55aa5c67d83178447e190128ab8fa3f88cdc
Derek Drill <derekdrillwebdev@gmail.com>
2025-10-30 15:59:28 -0400

    temp remove of caching (#70)

--- src/routes/game.ts
+++ src/routes/game.ts
@@ -4,13 +4,13 @@ import { checkCache, memcached } from '@cache';
 
 const router = Router();
 
-router.get('/:gameId', checkCache, async (req, res) => {
+router.get('/:gameId', async (req, res) => {
   const key = req.originalUrl;
   const data = await getGame(req, res);
 
-  memcached.set(key, JSON.stringify(data), { expires: 60 }, (err) => {
-    if (err) console.error(err);
-  });
+  // memcached.set(key, JSON.stringify(data), { expires: 60 }, (err) => {
+  //   if (err) console.error(err);
+  // });
 
   res.json(data);
 });
--- src/routes/teams.ts
+++ src/routes/teams.ts
@@ -4,24 +4,24 @@ import { checkCache, memcached } from '@cache';
 
 const router = Router();
 
-router.get('/', checkCache, async (req, res) => {
+router.get('/', async (req, res) => {
   const key = req.originalUrl;
   const data = await getTeams(req, res);
 
-  memcached.set(key, JSON.stringify(data), { expires: 60 }, (err) => {
-    if (err) console.error;
-  });
+  // memcached.set(key, JSON.stringify(data), { expires: 60 }, (err) => {
+  //   if (err) console.error;
+  // });
 
   res.json(data);
 });
 
-router.get('/current', checkCache, async (req, res) => {
+router.get('/current', async (req, res) => {
   const key = req.originalUrl;
   const data = await getTeamsCurrent(req, res);
 
-  memcached.set(key, JSON.stringify(data), { expires: 60 }, (err) => {
-    if (err) console.error;
-  });
+  // memcached.set(key, JSON.stringify(data), { expires: 60 }, (err) => {
+  //   if (err) console.error;
+  // });
 
   res.json(data);
 });
